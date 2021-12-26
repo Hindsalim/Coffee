93c36d2c951b0216e88af815b9ac37a54bb9f2de
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-12-26 15:05:52 +0300

    Add Modification map

--- Coffee/Coffee/Coffee/ViewController.swift
+++ Coffee/Coffee/Coffee/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
     //    @IBOutlet weak var name: UITextField!

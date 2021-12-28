7895ee91a5a2c7d2f1658c5c2ca33a89a33d72bd
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-12-28 21:07:02 +0300

    Add face the orders

---
+++ Coffee/ OrdersVc.swift
@@ -0,0 +1,16 @@
+//
+//  OldersVc.swift
+//  Coffee
+//
+//  Created by HIND12 on 05/05/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class OldersVC: UIViewController {
+
+    
+    @IBOutlet weak var listOrders: UITableView!
+    
+}
--- Coffee/OldersVc.swift
+++
@@ -1,8 +0,0 @@
-//
-//  OldersVc.swift
-//  Coffee
-//
-//  Created by HIND12 on 05/05/1443 AH.
-//
-
-import Foundation

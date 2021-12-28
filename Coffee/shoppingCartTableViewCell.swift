1ed42ae2c60a484cd1d1b309906277db949f8f11
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-12-29 02:22:22 +0300

    Modification code

--- Coffee/shoppingCartTableViewCell.swift
+++ Coffee/shoppingCartTableViewCell.swift
@@ -10,11 +10,20 @@ import UIKit
 class shoppingCartTableViewCell: UITableViewCell {
 
     @IBOutlet weak var imageProduct: UIImageView!
+    @IBOutlet weak var lblProductName: UILabel!
+    @IBOutlet weak var lblProductPrice: UILabel!
+    @IBOutlet weak var lblProductDescription: UILabel!
+    @IBOutlet weak var btnAddToCoffee: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    func setupcell(Product: UIImage,name: String,price: Double,description: String) {
+        imageProduct.image = Product
+        lblProductName.text = name
+        lblProductPrice.text = "\(price)SAR"
+        lblProductDescription.text = description
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 

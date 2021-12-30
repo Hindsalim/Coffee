abd71a25bda0c0841f43d1198dff0063091d3c25
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-12-30 16:40:26 +0300

    Add shoppingTVC

--- Coffee/shoppingTVC...swift
+++ Coffee/shoppingTVC...swift
@@ -8,26 +8,32 @@
 import Foundation
 import UIKit
 
+
+
 class shoppingTVC: UITableViewCell {
 
-    @IBOutlet weak var imageViewShopping: UIImageView!
+    @IBOutlet weak var imageViewProduct: UIImageView!
     @IBOutlet weak var nameProduct: UILabel!
     @IBOutlet weak var priceProduct: UILabel!
     @IBOutlet weak var describeProduct: UILabel!
     
-
-
-
-
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-    func configurecell(with: shopping) {
-        labelTitle.text = with.name
-        labelRating.text = "Rating: \(with.rate)"
-        labelCategory.text = getshoppingType(withType: TypeOfshopping.init(rawValue: Int(with.type)) ?? .action)
-        imageViewshopping.image = UIImage(named: with.imageURL ?? "1")
+    func configurecell(with: Product) {
+        nameProduct.text = with.name
+        priceProduct.text = "Product: \(with.price)"
+        describeProduct.text = with.describe
+        imageViewProduct.image = UIImage(named: with.imageName)
     }
+    
+    
+    @IBAction func addCart(_ sender: Any) {
+    }
+    func
+    @IBAction func like(_ sender: Any) {
+    }
+    
 }
